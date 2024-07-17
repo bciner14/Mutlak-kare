24adb4a8c30258ebe7f8ba6f9e95a8d371640f38
bciner14 <143399567+bciner14@users.noreply.github.com>
2024-07-17 11:33:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the integers: ");
+        string input = Console.ReadLine();
+        List<int> numbers = input.Split(' ').Select(int.Parse).ToList();
+
+        int differenceSum = 0;
+        int squaredDifferenceSum = 0;
+
+        foreach (int number in numbers)
+        {
+            if (number < 67)
+            {
+                differenceSum += (67 - number);
+            }
+            else
+            {
+                squaredDifferenceSum += (number - 67) * (number - 67);
+            }
+        }
+
+        Console.WriteLine($"Difference Sum (for numbers < 67): {differenceSum}");
+        Console.WriteLine($"Squared Difference Sum (for numbers >= 67): {squaredDifferenceSum}");
+    }
+}
